bc16f15f4e6bfe3fd6282348570293cbc00fa7dd
Rahul Bareja <rahul.bareja@daffodilsw.com>
2020-08-21 12:03:01 +0530

    added base layout

--- App.js
+++ App.js
@@ -6,108 +6,68 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
   StatusBar,
+  TouchableOpacity,
+  Text,
+  View,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
 
-const App: () => React$Node = () => {
+  const camRef = useRef();
+  const onCapturePress = () => {
+    // camRef.current.captureImage();
+  };
+  const onRecordPress = () => {
+    // camRef.current.recordVideo();
+  };
+  const onStopPress = () => {
+    // camRef.current.stopRecording();
+  };
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+      <SafeAreaView style={styles.flex}>
+        {/** Change View to your CamView */}
+        <View
+          style={styles.container}
+          ref={camRef}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={onCapturePress} style={styles.button}>
+            <Text>{'Capture'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onRecordPress} style={styles.button}>
+            <Text>{'Record'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onStopPress} style={styles.button}>
+            <Text>{'Stop'}</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  flex: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
   },
-  body: {
-    backgroundColor: Colors.white,
+  buttonContainer: {
+    flexDirection: 'row',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    flex: 1,
+    paddingVertical: 20,
+    alignItems: 'center',
   },
 });
 
